bf922bee912b28de1f99319f0d4b1e3b4d0ddb9e
kali <1352151710@qq.com>
2017-06-01 20:27:46 +0800

    helloworld!

--- shiyan/shiyan3.py
+++ shiyan/shiyan3.py
@@ -7,14 +7,15 @@ parser.add_argument("-o",dest="outputfile",type=str,default=None,)
 
 args=parser.parse_args()
 user_agent="Mozilla/5.0(Windows NT 10.0;Win64;x64;rv:53.0)Gecko/20100101 Firefox/53.0"
-if args.url.find("http://")!=-1:
-	print("find ")
-else:
+if args.url.find("http://")==-1:
 	args.url="http://"+args.url
-	print("not find!")
 headers={'User-Agent':user_agent}
 req=urllib.request.Request(args.url,headers=headers)
 with urllib.request.urlopen(req,timeout=30) as f:
 	html=f.read().decode("utf-8")
-	with open(args.outputfile,"w") as fp:
-		fp.write(html)
+	if args.outputfile:
+		with open(args.outputfile,"w") as fp:
+			fp.write(html)
+	else:
+		with open("spider.html","w") as fp:
+			fp.write(html)
